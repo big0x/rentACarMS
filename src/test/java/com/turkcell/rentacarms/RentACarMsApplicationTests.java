d64675497784fc50ebbc91186902351d7ad4525f
Mehmet Yurdakul <mehmetyurdakul.ym@gmail.com>
2022-02-18 02:46:55 +0300

    first commit

---
+++ src/test/java/com/turkcell/rentacarms/RentACarMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.rentacarms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
